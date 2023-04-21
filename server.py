41a5fdeb1f30c8c789b7ad0035b830d3755eea1a
Tim Johnson <armanijohnson1331@gmail.com>
2023-04-20 19:19:22 -0500

    Update server.py

--- server.py
+++ server.py
@@ -3,4 +3,4 @@ from flask_app.controllers import users_controller
 from flask_app.controllers import trees_controller
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5005)
+    app.run(debug=True, port=5000)
